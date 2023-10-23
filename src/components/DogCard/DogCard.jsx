c5f98f04d870dce452d25d942d136933ae178cc7
ignatiusaa <ignatius.a.ambrose@gmail.com>
2023-10-23 10:01:13 +0800

    feat: added showmore feature for card

--- src/components/DogCard/DogCard.jsx
+++ src/components/DogCard/DogCard.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import { useState } from 'react'
 
 const DogCard = ({dogData}) => {
+  const [showMore, setShowMore] = useState(false)
+
+  const toggleShowMore = () => {
+    setShowMore(!showMore);
+  }
   return (
     <div>
         <img src={dogData.image_link} alt={dogData.name} />
         <h2>{dogData.name}</h2>
+        <p>
+          {showMore}
+          <button onClick={toggleShowMore} className="see-more-link">
+            {showMore ? 'See Less' : 'See More Characteristics'}
+          </button>
+        </p>
+
+        {showMore && (
+          <>
+            <p>Good with Children: {dogData.good_with_children}</p>
+            <p>Good with Other Dogs: {dogData.good_with_other_dogs}</p>
+            <p>Shedding: {dogData.shedding}</p>
+            <p>Grooming: {dogData.grooming}</p>
+            <p>Drooling: {dogData.drooling}</p>
+            <p>Playfulness: {dogData.playfulness}</p>
+            <p>Protectiveness: {dogData.protectiveness}</p>
+            <p>Trainability: {dogData.trainability}</p>
+            <p>Energy: {dogData.energy}</p>
+            <p>Barking: {dogData.barking}</p>
+          </>
+        )}
     </div>
   )
 }

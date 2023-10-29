14c87b9e0dc4aed0a85acb995cd34f108c7754f6
ignatiusaa <ignatius.a.ambrose@gmail.com>
2023-10-30 06:54:30 +0800

    feat: added loading for search

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import {searchDogBreeds, getDogNames, addDogToWishList } from "../../Utilities/users-service"
 import DogCard from '../DogCard/DogCard';
 import WishListPage from '../../pages/WishListPage/WishListPage';
-import { Grid } from '@mui/material';
+import { Autocomplete, TextField, Grid, Button, CircularProgress } from '@mui/material';
 
 
 const SearchBar = () => {
@@ -10,6 +10,7 @@ const SearchBar = () => {
     const [searchResults, setSearchResults] = useState([]);
     const [dogBreeds, setDogBreeds] = useState([]);
     const [wishList, setWishList] = useState([]);
+    const [loading, setLoading] = useState(false)
 
     useEffect(() => {
         const fetchDogBreeds = async () => {
@@ -27,12 +28,14 @@ const SearchBar = () => {
 
 
     const handleSearch = async () => {
+        setLoading(true)
         try {
             const searchData = await searchDogBreeds(searchQuery);
             setSearchResults(searchData);
         } catch (error) {
             console.error(error.message);
         }
+        setLoading(false)
     }
 
     const handleAddToWishList = async (dogData) => {
@@ -47,41 +50,46 @@ const SearchBar = () => {
       }
 
   return (
-    <div>
-        <input
-            type="text"
-            placeholder="Search for dog breed"
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-        />
-        <select
-                value={searchQuery}
-                onChange={(e) => setSearchQuery(e.target.value)}
-            >
-                <option value="">Select a dog breed</option>
-                {dogBreeds.map((breed, index) => (
-                    <option key={index} value={breed}>
-                        {breed}
-                    </option>
-                ))}
-            </select>
-        <button onClick={handleSearch}>Search</button>
-        <Grid container spacing={2}>
-            {searchResults.map((result, index) => (
-            <Grid item key={index} xs={6} sm={4} md={3} lg={2}>
-                <DogCard
-                searchResult={result}
-                onAddToWishList={() => handleAddToWishList(result)}
-                showAddToWishList={true}
+    <Grid container justifyContent="center">
+       <Grid item sx={{ alignItems: 'center', p: 2 }}>
+                <Autocomplete
+                    options={dogBreeds}
+                    value={searchQuery}
+                    onChange={(e, newValue) => setSearchQuery(newValue)}
+                    renderInput={(params) => (
+                        <TextField {...params} label="Search for dog breed" sx={{ width: '200%' }} />
+                    )}
                 />
+                 <Grid item>
+                    <Button onClick={handleSearch}>
+                        Search
+                    </Button>
+                </Grid>
             </Grid>
-            ))}
-      </Grid>
-        <WishListPage 
-        wishList={wishList} 
-        setWishList={setWishList}
-        />
-    </div>
+            <Grid item xs={12} sx={{ mt: 2 }}>
+                {loading ? (
+                    <CircularProgress /> 
+                ) : (
+                    <Grid container spacing={2}>
+                        {searchResults.map((result, index) => (
+                            <Grid item key={index} xs={6} sm={4} md={3} lg={2}>
+                                <DogCard
+                                    searchResult={result}
+                                    onAddToWishList={() => handleAddToWishList(result)}
+                                    showAddToWishList={true}
+                                />
+                            </Grid>
+                        ))}
+                    </Grid>
+                )}
+            </Grid>
+        <Grid item xs={12} sx={{ mt: 2 }}>
+            <WishListPage 
+            wishList={wishList} 
+            setWishList={setWishList}
+            />
+        </Grid>
+    </Grid>     
   )
 }
 

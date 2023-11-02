3f5bf7a019d11b21bc71f63750bc7da5e4219254
ignatiusaa <ignatius.a.ambrose@gmail.com>
2023-11-02 11:07:11 +0800

    update searchbar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -63,6 +63,7 @@ const SearchBar = () => {
                     />
                 )}
                 freeSolo
+                onChangeCapture={(e) => setSearchQuery(e.target.value)}
             />
             </Grid>
                 <Button onClick={handleSearch}>
